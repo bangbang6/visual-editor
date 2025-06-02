f3c5801bac616255d3893fcb1cb782ed1949bebc
bangbang6 <577174667@qq.com>
2025-06-02 16:56:17 +0800

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ export type ICustomAxiosConfig = AxiosRequestConfig & {
   opName?: string;
 };
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = "http://www.loveyc.cc:9091/api";
 axios.interceptors.request.use((config: ICustomAxiosConfig) => {
   store.commit("startLoading", { opName: config.opName });
   store.commit("setError", { status: false, message: "" });
